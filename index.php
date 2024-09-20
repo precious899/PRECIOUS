2b2e7bd6800ec2868db2cab7115f46d6f183a89d
precious899 <2023abit1855f@kab.ac.ug>
2024-09-20 04:28:18 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="ORISHABA PRECIOUS";
+$DOB="24/03/2003";
+$Address="KANUNGU";
+$CYear=2024;
+$BYear=2003;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+CYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
